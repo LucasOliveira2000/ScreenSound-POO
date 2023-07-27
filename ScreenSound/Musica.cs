8eec6db8efac1a9fb63a8ebfe40a3e3534bd6909
Lucas Oliveira <123789449+LucasOliveira2000@users.noreply.github.com>
2023-07-27 20:11:11 -0300

    Update Musica.cs

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -5,20 +5,21 @@
         Artista = artista;
         Nome = nome;
     }
-
+    // Abaixo está todos os atributos da class Musica
     public string Nome { get; } //ctrl +r+r renomear em todos os locais
     public Banda Artista { get; } // get = Leitura
     public int Duracao { get; set; } // set = atribuição
-    public bool Disponivel { get;set;} // propriedade
+    public bool Disponivel { get;set;} // propriedade get e set
     public string DescricaoResumida =>
         $"A música {Nome} pertence à banda {Artista}"; //Propriedade somente leitura => Lambda
    
        
-    public void ExibirFichaTecnica()
+    public void ExibirFichaTecnica() // Agora temos um método que representa um comportamento de qualquer objeto da classe Musica
     {
         Console.WriteLine($"Nome: {Nome}");
         Console.WriteLine($"Artista: {Artista.Nome}");
         Console.WriteLine($"Duração: {Duracao}");
+        //Condição para verigicar se está disponivel ou não
         if (Disponivel)
         {
             Console.WriteLine("Disponível no Plano.");
